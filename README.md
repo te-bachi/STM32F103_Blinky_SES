f36709950af8c8bb4d09b96b03fd4a5bdcb1c97b
Andreas Bachmann <bachi@te-clan.ch>
2019-01-01 12:29:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# STM32F103_Blinky_SES
---
+++ Source/main.cpp
@@ -0,0 +1,100 @@
+/*********************************************************************
+*                    SEGGER Microcontroller GmbH                     *
+*                        The Embedded Experts                        *
+**********************************************************************
+*                                                                    *
+*            (c) 2014 - 2018 SEGGER Microcontroller GmbH             *
+*                                                                    *
+*           www.segger.com     Support: support@segger.com           *
+*                                                                    *
+**********************************************************************
+*                                                                    *
+* All rights reserved.                                               *
+*                                                                    *
+* Redistribution and use in source and binary forms, with or         *
+* without modification, are permitted provided that the following    *
+* conditions are met:                                                *
+*                                                                    *
+* - Redistributions of source code must retain the above copyright   *
+*   notice, this list of conditions and the following disclaimer.    *
+*                                                                    *
+* - Neither the name of SEGGER Microcontroller GmbH                  *
+*   nor the names of its contributors may be used to endorse or      *
+*   promote products derived from this software without specific     *
+*   prior written permission.                                        *
+*                                                                    *
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND             *
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,        *
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF           *
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           *
+* DISCLAIMED.                                                        *
+* IN NO EVENT SHALL SEGGER Microcontroller GmbH BE LIABLE FOR        *
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR           *
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT  *
+* OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;    *
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF      *
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT          *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  *
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH   *
+* DAMAGE.                                                            *
+*                                                                    *
+**********************************************************************
+
+-------------------------- END-OF-HEADER -----------------------------
+
+File    : main.c
+Purpose : Generic application start
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BlinkLed.h"
+#include "Timer.h"
+
+namespace
+{
+  // ----- Timing definitions -------------------------------------------------
+
+  // Keep the LED on for 2/3 of a second.
+  constexpr Timer::ticks_t BLINK_ON_TICKS = Timer::FREQUENCY_HZ * 3 / 4;
+  constexpr Timer::ticks_t BLINK_OFF_TICKS = Timer::FREQUENCY_HZ - BLINK_ON_TICKS;
+}
+
+/*********************************************************************
+*
+*       main()
+*
+*  Function description
+*   Application entry point.
+*/
+int main(void) {
+
+  Timer timer;
+  timer.start();
+
+  BlinkLed blinkLed;
+
+  // Perform all necessary initialisations for the LED.
+  blinkLed.powerUp();
+
+  uint32_t seconds = 0;
+
+  // Infinite loop
+  while (1)
+    {
+      blinkLed.turnOn();
+      timer.sleep(seconds== 0 ? Timer::FREQUENCY_HZ : BLINK_ON_TICKS);
+
+      blinkLed.turnOff();
+      timer.sleep(BLINK_OFF_TICKS);
+
+      ++seconds;
+
+      // Count seconds on the trace device.
+      printf("Second %u\n", seconds);
+    }
+}
+
+/*************************** End of file ****************************/
